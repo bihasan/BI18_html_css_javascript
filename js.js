9b70999f8130241931b26421b81cd32207bced25
bihasan <42807527+bihasan@users.noreply.github.com>
2019-03-18 00:49:16 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,64 @@
+/*var lives=3;
+
+var example="I am a variable.";
+document.write(example);
+
+example=" This is another example of same variable ";
+document.write(example);
+--------
+
+alert("Let's Begin The Challenge!");
+
+document.write("Congrats You Fixed This Line Of Code!");
+
+
+Console.log("Good Job Your Almost There!");
+
+Console.log("If this appears in the console then you have completed the challenge, Well Done!")
+----------------
+*/
+/*var VisitorAge = prompt("What is your age?");
+
+var VisitorName = prompt("What is your name?");
+document.write("Hello! " + VisitorName + " Your current age as of today is " + VisitorAge);
+window.alert("Hello! " + VisitorName + " Your current age as of today is " + VisitorAge);
+
+
+var hotText = 'MDN';
+var URL = 'https://developer.mozilla.org/';
+
+document.write('Click to return to ' + hotText.link(URL));
+*/
+/*---------------------------------------------------------------*/
+/*
+window.alert("Hello wolcome the page! :)");
+
+var VisitorName = prompt('what is you Name? ');
+
+var VisitorsFavouriteColor = prompt('What is your favourite color? ');
+
+document.write("Hello! " + VisitorName + "your favourite color is " +  VisitorsFavouriteColor);
+
+console.log("Program Colpleted");
+*/
+/*------------------------------------------*/
+
+/*
+var VigitorsAge = prompt("How many Years Have You Been Alive For?");
+
+var YearsAlive = parseInt(VigitorsAge);
+
+YearsAlive = YearsAlive * 365;
+
+document.write ("You have been alive for " + YearsAlive + " days");
+*/
+//working with the Math object- RNmunber-random number//
+
+var UserNumber = prompt("Pick your first number");
+UserNumber = parseInt(UserNumber);
+
+var RNnumber = Math.floor(Math.random() *UserNumber)+1;
+alert("Your Random Number is " + RNnumber);
+
+
+
